4a0701514df0d763233772f4d6f44d8e380161ae
Tejas Bobhate <tejas25789@gmail.com>
2017-05-17 18:49:39 +0530

    added interfacing routine for cuda_filter function

--- cuda_lib.cu
+++ cuda_lib.cu
@@ -18,6 +18,17 @@ __global__ void cuda_gray_kernel(unsigned char *b, unsigned char *g, unsigned ch
 
 }
 
+__global__ void cuda_filter_kernel(unsigned char *gray, float *filter, unsigned char *result, size_t rows, size_t columns, size_t filterWidth){
+    
+    size_t x = blockIdx.x * blockDim.x + threadIdx.x;
+    //size_t y = blockIdx.y * blockDim.y + threadIdx.y;
+    return;
+
+
+
+
+}
+
 extern "C" {
 void cuda_gray(unsigned char *a, unsigned char *b, unsigned char *c, unsigned char *d, size_t size)
 {
@@ -55,4 +66,41 @@ void cuda_gray(unsigned char *a, unsigned char *b, unsigned char *c, unsigned ch
     cudaFree(d_c);
     cudaFree(d_d);
 }
+
+void cuda_filter(unsigned char *gray, float *filter, unsigned char *result, size_t rows, size_t columns, size_t filterWidth){
+    
+    size_t imageSize = rows * columns;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    unsigned char *d_gray, *d_result;
+    float *d_filter;
+
+    cudaMalloc((void **)&d_filter, filterWidth * sizeof(float));
+    cudaMalloc((void **)&d_gray, imageSize * sizeof(char));
+    cudaMalloc((void **)&d_result, imageSize * sizeof(char));
+
+    cudaMemcpy(d_gray, gray, imageSize * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_result, result, imageSize * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_filter, filter, filterWidth * sizeof(float), cudaMemcpyHostToDevice);
+
+    cudaEventRecord(start);
+    cuda_filter_kernel <<<    >>> (d_gray, d_filter, d_result, rows, columns, filterWidth);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+    printf("Time on GPU : %f msec\n", milliseconds);
+
+    cudaMemcpy(d_result, result , imageSize * sizeof(char), cudaMemcpyDeviceToHost);
+
+    cudaFree(d_gray);
+    cudaFree(d_filter);
+    cudaFree(d_result);
+
+
+}
+
+
 }
--- cuda_test.py
+++ cuda_test.py
@@ -3,19 +3,16 @@ import ctypes, cv2
 import matplotlib.pyplot as plt 
 from ctypes import *
 
-# extract  function pointer in the shared object cuda_sum.so
+
 def get_cuda_gray():
     dll = ctypes.CDLL('./cuda_lib.so', mode=ctypes.RTLD_GLOBAL)
     func = dll.cuda_gray
     func.argtypes = [POINTER(c_ubyte), POINTER(c_ubyte), POINTER(c_ubyte), POINTER(c_ubyte), c_size_t]
     return func
 
-# create  function with get_cuda_sum()
 __cuda_gray = get_cuda_gray()
 
-# convenient python wrapper for __cuda_sum
-# it does all job with types convertation
-# from python ones to C++ ones
+
 def cuda_gray(a, b, c, d, size):
     a_p = a.ctypes.data_as(POINTER(c_ubyte))
     b_p = b.ctypes.data_as(POINTER(c_ubyte))
@@ -25,13 +22,28 @@ def cuda_gray(a, b, c, d, size):
 
 
 
+
+def get_cuda_filter():
+    dll = ctypes.CDLL('./cuda_lib.so', mode=ctypes.RTLD_GLOBAL)
+    func = dll.cuda_filter
+    func.argtypes = [POINTER(c_ubyte), POINTER(c_float), POINTER(c_ubyte), c_size_t, c_size_t, c_size_t]
+    return func
+
+__cuda_filter = get_cuda_filter()
+
+
+def cuda_filter(gray, filter_, result, rows, columns, filterWidth):
+    gray_p = gray.ctypes.data_as(POINTER(c_ubyte))
+    filter_p = filter_.ctypes.data_as(POINTER(c_float))
+    result_p = result.ctypes.data_as(POINTER(c_ubyte))
+    __cuda_filter(gray_p, filter_p, result_p, rows, columns, filterWidth)
+
+
+
+
 # testing, sum of two arrays of ones and output head part of resulting array
 if __name__ == '__main__':
 
-    # file_dir = os.path.dirname(os.path.realpath(__file__))
-    # image_path = os.path.join(file_dir, 'images')
-
-    # image_name = os.path.join(image_path, 'river.jpg')
     img = cv2.imread('river.jpeg')
     rows, columns = img.shape[:2]
     b,g,r = cv2.split(img)
@@ -47,3 +59,14 @@ if __name__ == '__main__':
     cv2.imwrite('gray.jpg', gray)
     # print 'Difference with OpenCV output : ', np.sum(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) - gray)
 
+    
+    gray=cv2.imread('river.jpeg', 0)
+    rows, columns = gray.shape[:2]
+    gray = gray.reshape((rows * columns , 1)).copy()
+    gray = gray.astype('uint8')
+    result = np.zeros(gray.shape).astype('uint8')
+    filterWidth = 3
+    filter_ = (np.ones((3,3))/9.0).astype('float32')
+    
+    cuda_filter(gray, filter_, result, rows, columns, filterWidth )
+    print 'Done'
